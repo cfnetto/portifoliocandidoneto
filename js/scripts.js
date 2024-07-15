afe1a1ea5c1b6d4141ea2e0aa0c481ad304ed189
netto <cfneto@edu.univali.br>
2024-07-15 15:02:21 -0300

    Atualizado HTML e CSS, adicionado script para mudança de tema claro/escuro

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  themeToggle.addEventListener('click', () => {
+      document.body.classList.toggle('light-mode');
+      themeToggle.textContent = document.body.classList.contains('light-mode') ? '🌙' : '🌞';
+  });
+});
